580e6f7ae2665bf36717590c95ec436cb340c16e
Yohei YASUKAWA <yasulab@gmail.com>
2015-07-17 15:57:20 +0900

    Create ch12_4_2 dir from ch12_4_1

---
+++ ch12_4_2/spec/support/utilities.rb
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
+
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Capybaraを使用していない場合にもサインインする。
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
