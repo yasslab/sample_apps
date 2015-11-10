293c11e56740adca135928781f9eb8817eaa2091
Yohei YASUKAWA <yasulab@gmail.com>
2015-11-10 11:31:25 +0900

    Limite 5MB instead of 1MB in ch12 in 4.2.2

--- 4_2_2/ch12/app/models/micropost.rb
+++ 4_2_2/ch12/app/models/micropost.rb
@@ -3,16 +3,15 @@ class Micropost < ActiveRecord::Base
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
-  validates :content, presence: true, 
+  validates :content, presence: true,
                         length: { maximum: 140 }
-  
   validate  :picture_size
-  
+
   private
 
     # アップロード画像のサイズを検証する
     def picture_size
-      if picture.size > 1.megabytes
+      if picture.size > 5.megabytes
         errors.add(:picture, "should be less than 5MB")
       end
     end
