b45bd4f1bb3d68c59fc233fe585abc87038f7c33
Yohei YASUKAWA <yasulab@gmail.com>
2016-03-13 15:31:48 +0900

    Finish ch05

--- 4_2_2/ch05/config/routes.rb
+++ 4_2_2/ch05/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
+  get  'help'    => 'static_pages#help'
+  get  'about'   => 'static_pages#about'
+  get  'contact' => 'static_pages#contact'
+  get  'signup'  => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- 4_2_2/ch05/test/controllers/users_controller_test.rb
+++ 4_2_2/ch05/test/controllers/users_controller_test.rb
@@ -5,5 +5,4 @@ class UsersControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
-
 end
