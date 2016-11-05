eb7983677af7da42cb4dc9e1fd66f67baae5b7fc
Yohei Yasukawa <yasulab@gmail.com>
2016-11-05 00:37:18 +0000

    Add ch08 for 4e

---
+++ ch08/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user
+    else
+      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
---
+++ ch08/app/helpers/users_helper.rb
@@ -0,0 +1,9 @@
+module UsersHelper
+  # 引数で与えられたユーザーのGravatar画像を返す
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+end
