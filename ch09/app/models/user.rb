bddc8eab2fb5fbe928f2484404696b767bff24c6
Yohei Yasukawa <yasulab@gmail.com>
2016-11-06 00:59:14 +0000

    Add ch09 for 4e

---
+++ ch09/app/models/user.rb
@@ -0,0 +1,46 @@
+class User < ApplicationRecord
+  attr_accessor :remember_token
+
+  before_save { self.email = email.downcase }
+  validates :name,  presence: true, 
+                      length: { maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                      length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX },
+                  uniqueness: { case_sensitive: false }
+  
+  has_secure_password
+  validates :password, presence: true,
+                         length: { minimum: 6},
+                      allow_nil: true
+
+  # 与えられた文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+end
---
+++ ch09/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  root 'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
+end
