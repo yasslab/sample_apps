0c16721925b1689d0c44e166972be686e4f3e643
Yusuke Yamane <yusukeyamane01@gmail.com>
2017-06-15 18:31:45 +0900

    Add it

--- 5_0_0_1/ch06/app/controllers/users_controller.rb
+++ 5_0_0_1/ch06/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- 5_0_0_1/ch06/config/routes.rb
+++ 5_0_0_1/ch06/config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  resources :users
 end
