27d93ded3fb104f3625ed03c9a58fa72d41873e1
Yusuke Yamane <yusukeyamane01@gmail.com>
2017-06-18 16:04:41 +0900

    finish

--- 5_0_0_1/ch06/app/controllers/users_controller.rb
+++ 5_0_0_1/ch06/app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "hey"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+        :password_confirmation)
+    end
 end
--- 5_0_0_1/ch06/config/routes.rb
+++ 5_0_0_1/ch06/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  post  '/signup',  to: 'users#create'
   resources :users
 end
