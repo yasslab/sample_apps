bbe8a87f3328c0e6c48f6ef7ce8c5446daf6487c
Yohei Yasukawa <yasulab@gmail.com>
2017-08-22 16:27:28 +0900

    Add sample_app of ch07 to Rails 5.1.2 directory

---
+++ 5_1_2/ch07/app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+class StaticPagesController < ApplicationController
+  def home
+    # app/views/リソース名/アクション名.html.erb
+    # app/views/static_pages/home.html.erb
+  end
+
+  def help
+  end
+  
+  def about
+    # 'app/views/static_pages/about.html.erb'
+  end
+  
+  def contact
+    # app/views/static_pages/contact.html.erb'
+  end
+end
---
+++ 5_1_2/ch07/app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    # => app/views/users/show.html.erb
+    # debugger
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save # => Validation
+      # Sucess
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # GET "/users/#{@user.id}" => show
+    else
+      # Failure
+      render 'new'      
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :password, 
+      :password_confirmation)
+  end
+end
