594050d5bda73e273f9f72bbfa69c60e4ddd6d84
Yoshiaki Oshiro <himajin315@gmail.com>
2020-02-25 14:48:19 +0900

    Add 6.0.0 ch07

---
+++ 6_0_0/ch07/app/helpers/users_helper.rb
@@ -0,0 +1,9 @@
+module UsersHelper
+
+  # 引数で与えられたユーザーのGravatar画像を返す
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+end
