a050398fc995c4f4f127814c0b22a7adc877649f
Yohei Yasukawa <yohei@yasslab.jp>
2021-10-14 13:52:00 +0900

    Fix #54: Add missing '{ case_sensitive: false }' option

--- 6_0_0/ch06/app/models/user.rb
+++ 6_0_0/ch06/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+                    uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 end
