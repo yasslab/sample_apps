336ca3eb2bccb639b66aeb54b43845ae77dcdc62
yoshiharu suzuki <suzuki02yo@gmail.com>
2021-12-11 17:20:21 +0900

    delete coment

--- 6_0_0/ch14/db/migrate/20211211015242_create_notifications.rb
+++ 6_0_0/ch14/db/migrate/20211211015242_create_notifications.rb
@@ -2,7 +2,6 @@ class CreateNotifications < ActiveRecord::Migration[6.0]
   def change
     create_table :notifications do |t|
       t.references :user, null: false, foreign_key: true
-      # t.string :event, null: false
       t.bigint :target_id, null: false
       t.integer :event, null: false
       t.boolean :read, null: false, default: false
