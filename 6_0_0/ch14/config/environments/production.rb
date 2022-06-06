012c4666a924281a6251e884e1426a256731b549
Yoshiaki Oshiro <himajin315@gmail.com>
2022-06-06 17:11:42 +0900

    Rails6.0 の ch13, ch14 を更新

--- 6_0_0/ch14/config/environments/production.rb
+++ 6_0_0/ch14/config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # アップロードされたファイルをAWSに保存する
-  # config.active_storage.service = :amazon
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'stark-earth-94189.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
