19dab15bb5ad0e7371bc45b54749e8204510cee4
rakuda <rakuda.san.desu@gmail.com>
2022-11-25 16:36:42 +0900

    Railsを7.0.4に更新

--- 7_0/ch05/app/helpers/application_helper.rb
+++ 7_0/ch05/app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     if page_title.empty?                              # 論理値テスト
       base_title                                      # 暗黙の戻り値
     else
-      page_title + " | " + base_title                 # 文字列の結合
+      "#{page_title} | #{base_title}"                 # 文字列の結合
     end
   end
 end
