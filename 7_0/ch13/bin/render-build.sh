2da8f3d4d24c2ea11638e6b737abd18956a5053e
rakuda <rakuda.san.desu@gmail.com>
2022-12-12 16:32:12 +0900

    rakeコマンドをrailsコマンドに変更した

--- 7_0/ch13/bin/render-build.sh
+++ 7_0/ch13/bin/render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate:reset
-bundle exec rake db:seed
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate:reset
+bundle exec rails db:seed
